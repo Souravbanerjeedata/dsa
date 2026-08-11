7ca8ed291481ebcfdcaeafac36c29ede9a63cb55
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-11 14:17:31 +0530

    Search Insert Position (LeetCode 35)

--- Leetcode/Binary Search/main.js
+++ Leetcode/Binary Search/main.js
@@ -22,3 +22,24 @@ const search = (nums, target) => {
 
 // console.log(search([-1, 0, 3, 5, 9, 12], 9));
 // console.log(search([-1, 0, 3, 5, 9, 12], 2));
+
+// https://leetcode.com/problems/search-insert-position/
+
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
