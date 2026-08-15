9c6415a5aa0a0151d057df023b64186b050c6b51
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-15 15:27:33 +0530

    leetcode for arrays & hashing

--- Leetcode/Arrays & Hashing/main.py
+++ Leetcode/Arrays & Hashing/main.py
@@ -10,4 +10,58 @@ class Solution:
                     return [i, j]
         return []
 
-# Sorting:
+# One-pass Hash Table:
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+        return []
+
+# https://leetcode.com/problems/contains-duplicate/description/
+
+# using hash map
+def containDuplicate(nums):
+    seen = {}
+
+    for i, num in enumerate(nums):
+        if num in seen:
+            return True
+        
+        seen[num] = i
+    return False
+
+# using hash set
+def containDuplicate2(nums):
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+def maxProfit(prices):
+    left, right = 0, 1
+    maxProfit = 0
+
+    while right < len(prices):
+        if prices[left] < prices[right]:
+            profit = prices[right] - prices[left]
+            maxProfit = max(maxProfit, profit)
+        else:
+            left = right
+        right += 1
+
+    return maxProfit
+
