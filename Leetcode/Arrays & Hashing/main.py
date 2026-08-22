7b43ec4b70206295d7b4b4b68cb0da6695ef41c8
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-08-22 14:23:30 +0530

    leetcode - top-k-frequent-elements

--- Leetcode/Arrays & Hashing/main.py
+++ Leetcode/Arrays & Hashing/main.py
@@ -135,7 +135,7 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
     count = {}
 
     for num in nums:
-        count[num] = count.get(num, 0) + 1\
+        count[num] = count.get(num, 0) + 1
 
     arr = []
     for num, cnt in count.items():
@@ -143,7 +143,8 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
     arr.sort()
 
     res = []
-    while res < k:
+    while len(res) < k:
         res.append(arr.pop()[1])
-
     return res
+
+# Bucket Sort
